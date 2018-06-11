1da40222aaf0be5210c2e8ecd3fbb3dc469b6658
BRENDA-ACER\Brenda <brnjdb@gmail.com>
2018-06-11 09:42:09 +0700

    Bersih2

--- src/main/java/com/piterpan/sipr/Interface/GrupInter.java
+++
@@ -1,9 +0,0 @@
-package com.piterpan.sipr.Interface;
-
-import com.piterpan.sipr.Model.Grup;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GrupInter extends JpaRepository<Grup, Integer> {
-}
--- src/main/java/com/piterpan/sipr/Interface/JadwalInter.java
+++
@@ -1,6 +0,0 @@
-package com.piterpan.sipr.Interface;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//public interface JadwalInter extends JpaRepository<>{
-//}
